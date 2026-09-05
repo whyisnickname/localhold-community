7d50dc1b1c8f49299d457d3658eff6845132d361
whyisnickname <320551152+whyisnickname@users.noreply.github.com>
2026-09-05 19:16:39 +0300

    Publish community snapshot from a1bb37db9e91

--- packages/vault_native/example/ios/RunnerTests/RunnerTests.swift
+++ packages/vault_native/example/ios/RunnerTests/RunnerTests.swift
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: MPL-2.0
-import Flutter
-import UIKit
+
+import Foundation
 import XCTest
-
-// If your plugin has been explicitly set to "type: .dynamic" in the Package.swift,
-// you will need to add your plugin as a dependency of RunnerTests within Xcode.
-
 @testable import localhold_key_bridge
 
-// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
-//
-// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+final class RunnerTests: XCTestCase {
+  func testD08WallClockAndShortcutAllowlist() {
+    let features = IOSPlatformFeatures()
+    let gap = features.resolveWallClock(WallClockRequest(
+      year: 2026,
+      month: 3,
+      day: 29,
+      hour: 2,
+      minute: 30,
+      timeZoneId: "Europe/Berlin"
+    ))
+    XCTAssertEqual(gap.resolution, .gapAdjusted)
+    XCTAssertNil(gap.error)
 
-class RunnerTests: XCTestCase {
-
-  func testGetPlatformVersion() {
-    let plugin = LocalholdKeyBridgePlugin()
-
-    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
-
-    let resultExpectation = expectation(description: "result block must be called.")
-    plugin.handle(call) { result in
-      XCTAssertEqual(result as! String, "iOS " + UIDevice.current.systemVersion)
-      resultExpectation.fulfill()
-    }
-    waitForExpectations(timeout: 1)
+    XCTAssertFalse(features.acceptLauncherShortcut("localhold.reveal"))
+    XCTAssertTrue(features.acceptLauncherShortcut("localhold.lock"))
+    XCTAssertEqual(features.consumeLauncherAction().action, .lock)
+    XCTAssertEqual(features.consumeLauncherAction().action, .none)
   }
 
+  func testD08InlineShareLimitsFailBeforeFilesystemAccess() {
+    XCTAssertFalse(IOSInboundShareStager.stageData(Data(), kind: .text))
+    XCTAssertFalse(
+      IOSInboundShareStager.stageData(Data(count: 64 * 1024 + 1), kind: .url)
+    )
+  }
 }
